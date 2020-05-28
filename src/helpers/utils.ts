5be3daf3b2817e6d67af785293ba83c5e4c3f180
Daniel Choudhury <dannychoudhury@gmail.com>
2020-05-28 14:00:42 +0100

    Set default format

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -25,6 +25,7 @@ export const commonFlags = {
   }), // dont upload
   format: flags.string({
     options: Object.values(CopyFormats),
+    default: CopyFormats.URL,
   }),
   nocopy: flags.boolean({
     default: false,

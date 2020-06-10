8d34660119e8ce0a1fda7ce81b9f658e2036319e
Ingmaras Keleras <as@ingmaras.com>
2020-06-10 17:56:49 +0100

    Confirm uploads

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -32,3 +32,10 @@ export const commonFlags = {
     helpLabel: `--nocopy ${chalk.grey('Disable copying to clipboard')}`,
   }),
 }
+
+export const bytesToSize = (bytes: number): string => {
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  if (bytes == 0) return '0 Byte'
+  var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i]
+}

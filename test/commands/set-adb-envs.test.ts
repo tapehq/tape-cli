b78c311f6fa97ab83b10e547db9b1420a745d32a
Daniel Choudhury <dannychoudhury@gmail.com>
2020-07-22 13:54:04 +0100

    Add tests for adb env hook

--- test/commands/set-adb-envs.test.ts
+++ test/commands/set-adb-envs.test.ts
@@ -1,9 +1,31 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
 
-describe('hooks', () => {
+describe('adb env hook', () => {
+  // Override path for testing
+  process.env.ANDROID_HOME = ''
+
   test
-  .stdout()
-  .hook('init', {id: 'mycommand'})
-  .do(output => expect(output.stdout).to.contain('example hook running mycommand'))
-  .it('shows a message')
+    .stdout()
+    .hook('prerun', { id: 'mycommand' })
+    .do((output) => {
+      expect(output.stdout).to.be.empty
+      expect(process.env.ANDROID_HOME).to.be.not.empty
+    })
+    .it('Adds ANDROID_HOME env variable')
+})
+
+describe('Adds adb to path if ANDROID_HOME present', () => {
+  // Override path for testing
+  process.env.ANDROID_HOME = 'XXX/TEST/PATH/ONLY'
+  // eslint-disable-next-line no-useless-escape
+  const PLATFORM_TOOL_REGEX = /XXX\/TEST\/PATH\/ONLY[\/\\]platform-tools/
+
+  test
+    .stdout()
+    .hook('prerun', { id: 'mycommand' })
+    .do((output) => {
+      expect(output.stdout).to.be.empty
+      expect(process.env.PATH).to.match(PLATFORM_TOOL_REGEX)
+    })
+    .it('Updates the path with platform tools')
 })
